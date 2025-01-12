94c3eb22db8e96c1c850c056a05c8ad0f7bf43ae
Владислав Соголаев <yaroslavsogolaev@yandex.ru>
2025-01-12 17:25:03 +0300

    Update next.config.mjs with environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true, // Включение строгого режима React
+    env: {
+      MONGO_URI: process.env.MONGO_URI, // Подключение переменной MONGO_URI из окружения
+    },
+    experimental: {
+      appDir: true, // Включение поддержки папки `/app` (если используется)
+    },
+  };
+  
+  export default nextConfig;
+  
